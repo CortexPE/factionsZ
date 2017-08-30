7a864d09963179b9cecaaeb98daa0e145eab5c86
_EschieEsh_ <Rio3000@abv.bg>
2017-08-30 10:52:33 +0300

    Update Commands.php

--- src/EschieEsh/factionsZ/Commands.php
+++ src/EschieEsh/factionsZ/Commands.php
@@ -11,80 +11,80 @@ use EschieEsh\factionsZ\subcommand\CreateSubCommand;
 class Commands extends PluginCommand
 {
 	/** @var SubCommand[] */
-	private $subCommands = [];
+    private $subCommands = [];
 	/** @var SubCommand[] */
-	private $aliasSubCommands = [];
+    private $aliasSubCommands = [];
 	/** @var factionsZ */
-	private $plugin;
+    private $plugin;
     
-	public function __construct(factionsZ $plugin)
+    public function __construct(factionsZ $plugin)
     {
-		$this->plugin = $plugin;
-		parent::__construct($plugin->getLanguage()->get("command.name"), $plugin);
-		$this->setPermission("factionsZ.command");
-		$this->setAliases([$plugin->getLanguage()->get("command.alias")]);
-		$this->setDescription($plugin->getLanguage()->get("command.desc"));
-		$this->setUsage($plugin->getLanguage()->get("command.usage"));
-		$this->loadSubCommand(new CreateSubCommand($plugin, "create", $this));
-        /// register commands
-	}
+        $this->plugin = $plugin;
+        parent::__construct($plugin->getLanguage()->get("command.name"), $plugin);
+        $this->setPermission("factionsZ.command");
+        $this->setAliases([$plugin->getLanguage()->get("command.alias")]);
+        $this->setDescription($plugin->getLanguage()->get("command.desc"));
+        $this->setUsage($plugin->getLanguage()->get("command.usage"));
+        $this->loadSubCommand(new CreateSubCommand($plugin, "create", $this));
+        
+        $this->plugin->getLogger()->debug("Commands Registered to factionsZ");
+    }
 	/**
 	 * @return SubCommand[]
 	 */
-	public function getCommands(): array
+    public function getCommands(): array
     {
-		return $this->subCommands;
-	}
+        return $this->subCommands;
+    }
 	/**
 	 * @param SubCommand $command
 	 */
-	private function loadSubCommand(SubCommand $command)
+    private function loadSubCommand(SubCommand $command)
     {
-		$this->subCommands[$command->getName()] = $command;
-		if ($command->getAlias() != "")
+        $this->subCommands[$command->getName()] = $command;
+        if ($command->getAlias() != "")
         {
-			$this->aliasSubCommands[$command->getAlias()] = $command;
-		}
-	}
+            $this->aliasSubCommands[$command->getAlias()] = $command;
+        }
+    }
 	/**
 	 * @param CommandSender $sender
 	 * @param string $alias
 	 * @param string[] $args
 	 * @return bool
 	 */
-	public function execute(CommandSender $sender, string $alias, array $args)
-	{
-		if (!isset($args[0]))
+    public function execute(CommandSender $sender, string $alias, array $args){
+        if (!isset($args[0]))
         {
-			return false;
-		}
-		$subCommand = strtolower(array_shift($args));
-		if (isset($this->subCommands[$subCommand]))
+            return false;
+        }
+        $subCommand = strtolower(array_shift($args));
+        if (isset($this->subCommands[$subCommand]))
         {
-			$command = $this->subCommands[$subCommand];
-		} 
+            $command = $this->subCommands[$subCommand];
+        } 
         elseif (isset($this->aliasSubCommands[$subCommand]))
         {
-			$command = $this->aliasSubCommands[$subCommand];
-		} 
+            $command = $this->aliasSubCommands[$subCommand];
+        } 
         else
         {
-			$sender->sendMessage(TextFormat::RED . $this->plugin->getLanguage()->get("command.unknown"));
-			return true;
-		}
+            $sender->sendMessage(TextFormat::RED . $this->plugin->getLanguage()->get("command.unknown"));
+            return true;
+        }
         
-		if ($command->canUse($sender))
+        if ($command->canUse($sender))
         {
-			if (!$command->execute($sender, $args))
+            if (!$command->execute($sender, $args))
             {
-				$usage = $this->plugin->getLanguage()->translateString("subcommand.usage", [$command->getUsage()]);
-				$sender->sendMessage($usage);
-			}
-		} 
+                $usage = $this->plugin->getLanguage()->translateString("subcommand.usage", [$command->getUsage()]);
+                $sender->sendMessage($usage);
+            }
+        } 
         else
         {
-			$sender->sendMessage(TextFormat::RED . $this->plugin->getLanguage()->get("command.unknown"));
-		}
-		return true;
-	}
+            $sender->sendMessage(TextFormat::RED . $this->plugin->getLanguage()->get("command.unknown"));
+        }
+        return true;
+    }
 }
