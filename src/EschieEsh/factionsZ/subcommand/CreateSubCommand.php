38b64280cdc354823f393d45bf1bbe43b02b8799
_EschieEsh_ <Rio3000@abv.bg>
2017-08-30 10:22:48 +0300

    Add files via upload

---
+++ src/EschieEsh/factionsZ/subcommand/CreateSubCommand.php
@@ -0,0 +1,61 @@
+<?php
+namespace EschieEsh\factionsZ\subcommand;
+
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\CommandSender;
+
+class CreateSubCommand extends SubCommand
+{
+	/**
+	 * @param CommandSender $sender
+	 * @return bool
+	 */
+	public function canUse(CommandSender $sender)
+    {
+		return $sender->hasPermission("factionsZ.command.create");
+	}
+	/**
+	 * @param CommandSender $sender
+	 * @param string[] $args
+	 * @return bool
+	 */
+	public function execute(CommandSender $sender, array $args){
+		if (empty($args))
+        {
+			return false;
+		}
+        
+        $faction = $args[0];
+        
+		if (!(ctype_alnum($faction))) 
+        {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("error.create.notalphanum"));
+			return true;
+        }
+        if(strlen($faction) < 3 || strlen($faction) > 15)
+        {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("error.create.namelength"));
+			return true;
+        }
+        
+        if ($this->getPlugin()->getFaction($faction) !== false)
+        {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("error.create.infaction"));
+			return true;
+        }
+        
+        if ($this->getPlugin()->factionExists($faction))
+        {
+            
+			$sender->sendMessage(TextFormat::RED . $this->translateString("error.create.exists"));
+			return true;
+		}
+        
+        $player = $sender->getName();
+        
+        $this->getPlugin()->createFaction($player, $faction);
+        $sender->sendMessage(TextFormat::LIGHT_GREEN . $this->translateString("create.success"));
+		return true;
+	}
+}
