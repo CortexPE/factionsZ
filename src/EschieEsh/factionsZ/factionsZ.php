5d070d26b36e8bebe20deea5847f0c5ec404d9a7
_EschieEsh_ <Rio3000@abv.bg>
2017-08-30 10:54:54 +0300

    Update factionsZ.php

--- src/EschieEsh/factionsZ/factionsZ.php
+++ src/EschieEsh/factionsZ/factionsZ.php
@@ -7,64 +7,64 @@ use pocketmine\Command\CommandSender;
 class factionsZ extends PluginBase
 {
     /** @var DataProvider $dataProvider */
-	private $dataProvider = null;
+    private $dataProvider = null;
     
     /** @var BaseLang $baseLang */
-	private $baseLang = null;
+    private $baseLang = null;
     
     /**
-	 * @api
-	 * @return BaseLang
-	 */
-	public function getLanguage(): BaseLang
+    * @api
+    * @return BaseLang
+    */
+    public function getLanguage(): BaseLang
     {
-		return $this->baseLang;
-	}
+        return $this->baseLang;
+    }
     
     public function createFaction($player, $faction)
     {
         $this->dataProvider->createFaction($player, $faction);
     }
-	/**
-	 * @api
-	 * @return DataProvider
-	 */
-	public function getProvider(): DataProvider
+    /**
+    * @api
+    * @return DataProvider
+    */
+    public function getProvider(): DataProvider
     {
-		return $this->dataProvider;
-	}
+        return $this->dataProvider;
+    }
     
     public function onEnable()
     {
         @mkdir($this->getDataFolder());
-		$this->getLogger()->notice("Loading...");
-		$this->saveDefaultConfig();
-		$this->reloadConfig();
+        $this->getLogger()->notice("Loading...");
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
 		
-		$lang = $this->getConfig()->get("language", BaseLang::FALLBACK_LANGUAGE);
-		$this->baseLang = new BaseLang($lang, $this->getFile() . "resources/");
+        $lang = $this->getConfig()->get("language", BaseLang::FALLBACK_LANGUAGE);
+        $this->baseLang = new BaseLang($lang, $this->getFile() . "resources/");
 
-		switch (strtolower($this->getConfig()->get("DataProvider")))
+        switch (strtolower($this->getConfig()->get("DataProvider")))
         {
-			case "sqlite":
-			case "sqlite3":  
-			default:
-				$this->dataProvider = new SQLiteDataProvider($this); /// yaml, others
-				break;
-		}
+            case "sqlite":
+            case "sqlite3":  
+            default:
+                $this->dataProvider = new SQLiteDataProvider($this); /// yaml, others
+                break;
+        }
 		
 		
-		//$this->getServer()->getPluginManager()->registerEvents($eventListener, $this); // For listeners, ffs
+        //$this->getServer()->getPluginManager()->registerEvents($eventListener, $this); // For listener
 		
-		$this->getServer()->getCommandMap()->register(Commands::class, new Commands($this));
-		$this->getLogger()->notice(TF::GREEN . "Enabled!");
+        $this->getServer()->getCommandMap()->register(Commands::class, new Commands($this));
+        $this->getLogger()->notice(TF::GREEN . "Enabled!");
     }
     
     public function onDisable()
     {
         if ($this->dataProvider !== null)
         {
-			$this->dataProvider->close();
-		}
+            $this->dataProvider->close();
+        }
     }
 }
