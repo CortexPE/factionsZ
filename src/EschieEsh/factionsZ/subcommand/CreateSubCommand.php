00060e39179396433759789c19afe480201bb370
_EschieEsh_ <Rio3000@abv.bg>
2017-08-30 11:01:02 +0300

    Update CreateSubCommand.php

--- src/EschieEsh/factionsZ/subcommand/CreateSubCommand.php
+++ src/EschieEsh/factionsZ/subcommand/CreateSubCommand.php
@@ -7,55 +7,56 @@ use pocketmine\command\CommandSender;
 
 class CreateSubCommand extends SubCommand
 {
-	/**
-	 * @param CommandSender $sender
-	 * @return bool
-	 */
-	public function canUse(CommandSender $sender)
+    /**
+    * @param CommandSender $sender
+    * @return bool
+    */
+    public function canUse(CommandSender $sender)
     {
-		return $sender->hasPermission("factionsZ.command.create");
-	}
-	/**
-	 * @param CommandSender $sender
-	 * @param string[] $args
-	 * @return bool
-	 */
-	public function execute(CommandSender $sender, array $args){
-		if (empty($args))
+        return $sender->hasPermission("factionsZ.command.create");
+    }
+    /**
+    * @param CommandSender $sender
+    * @param string[] $args
+    * @return bool
+    */
+    public function execute(CommandSender $sender, array $args)
+    {
+        if (empty($args))
         {
-			return false;
-		}
+            return false;
+        }
         
         $faction = $args[0];
         
-		if (!(ctype_alnum($faction))) 
+        if (!(ctype_alnum($faction))) 
         {
             $sender->sendMessage(TextFormat::RED . $this->translateString("error.create.notalphanum"));
-			return true;
+            return true;
         }
         if(strlen($faction) < 3 || strlen($faction) > 15)
         {
             $sender->sendMessage(TextFormat::RED . $this->translateString("error.create.namelength"));
-			return true;
+            return true;
         }
         
         if ($this->getPlugin()->getFaction($faction) !== false)
         {
             $sender->sendMessage(TextFormat::RED . $this->translateString("error.create.infaction"));
-			return true;
+            return true;
         }
         
         if ($this->getPlugin()->factionExists($faction))
         {
             
-			$sender->sendMessage(TextFormat::RED . $this->translateString("error.create.exists"));
-			return true;
-		}
+            $sender->sendMessage(TextFormat::RED . $this->translateString("error.create.exists"));
+            return true;
+        }
         
         $player = $sender->getName();
         
         $this->getPlugin()->createFaction($player, $faction);
         $sender->sendMessage(TextFormat::LIGHT_GREEN . $this->translateString("create.success"));
-		return true;
-	}
+        return true;
+    }
 }
